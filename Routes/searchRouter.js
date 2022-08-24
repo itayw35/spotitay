237fb3a97fd6f5c96e4e234b7a02c4887aee8871
Itay <weinbergitay@gmail.com>
2022-08-24 12:07:05 +0300

    setting allow access

--- Routes/searchRouter.js
+++ Routes/searchRouter.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const searchLogic = require("../BL/searchLogic");
 const jwtAuth = require("../middleware/auth");
 
-router.get("/:query", jwtAuth, async (req, res) => {
+router.get("/:query", jwtAuth, async (req, res, next) => {
   try {
     const inp = req.params.query;
     const searchResult = await searchLogic.searchYT(inp);
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      " Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
     res.status(200).send(searchResult);
   } catch (err) {
