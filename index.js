c0e9ecbb54b9f2dacf8cbb681c8f924830a7e653
Itay <weinbergitay@gmail.com>
2022-08-25 23:31:50 +0300

    d

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require("express"),
   app = express(),
   PORT = process.env.PORT;
 app.use(require("cors")());
-app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
@@ -13,6 +12,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(express.json());
 app.listen(PORT, () => console.log("connection succeeded!"));
 app.use("/", require("./Routes/mainRouter"));
 require("./DL/db").myConnect();
