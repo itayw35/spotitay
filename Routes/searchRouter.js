9fa571987849178b18abadbedd9701ab98fd4943
Itay <weinbergitay@gmail.com>
2022-08-25 19:15:54 +0300

    access control allow

--- Routes/searchRouter.js
+++ Routes/searchRouter.js
@@ -7,12 +7,6 @@ router.get("/:query", jwtAuth, async (req, res, next) => {
   try {
     const inp = req.params.query;
     const searchResult = await searchLogic.searchYT(inp);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      " Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
     res.status(200).send(searchResult);
   } catch (err) {
     console.log(err);
--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const express = require("express"),
   app = express(),
   PORT = process.env.PORT;
 app.use(require("cors")());
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    " Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.listen(PORT, () => console.log("connection succeeded!"));
 app.use("/", require("./Routes/mainRouter"));
