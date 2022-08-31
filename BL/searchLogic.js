93986781cd7635ab3fcaa505cbb78292737032a5
Itay <weinbergitay@gmail.com>
2022-09-01 02:20:00 +0300

    log

--- BL/searchLogic.js
+++ BL/searchLogic.js
@@ -15,7 +15,6 @@ async function searchYT(inp) {
   const res = await axios.request(options).catch(function (error) {
     console.error(error);
   });
-  console.log(res.data.results);
   return res.data.results;
 }
 module.exports = { searchYT };
